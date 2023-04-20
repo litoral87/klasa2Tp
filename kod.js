f40adbac0b5e6e4ceb572ea6249903581c4e86bb
litoral87 <palontom87@wp.pl>
2023-04-20 17:35:32 +0200

    gotowy projekt ale nie wydajny

--- kod.js
+++ kod.js
@@ -7,26 +7,51 @@ suwak.addEventListener("click", ()=>{
 
     let value2 = suwak.value;
     console.log(value2);
+    if(value2==0){
+        serce[0].style.filter = `grayscale(100)`;
+        serce[1].style.filter = `grayscale(100)`;
+        serce[2].style.filter = `grayscale(100)`;
+        serce[3].style.filter = `grayscale(100)`;
+    serce[4].style.filter = `grayscale(100)`;
+    }
    if(value2==1)
    {
     serce[0].style.filter = `grayscale(0)`;
+    serce[1].style.filter = `grayscale(100)`;
+    serce[2].style.filter = `grayscale(100)`;
+    serce[3].style.filter = `grayscale(100)`;
+    serce[4].style.filter = `grayscale(100)`;
    }
    if(value2==2)
    {
    
+    serce[0].style.filter = `grayscale(0)`;
     serce[1].style.filter = `grayscale(0)`;
+    serce[2].style.filter = `grayscale(100)`;
+    serce[3].style.filter = `grayscale(100)`;
+    serce[4].style.filter = `grayscale(100)`;
    }
    if(value2==3)
    {
+    serce[0].style.filter = `grayscale(0)`;
+    serce[1].style.filter = `grayscale(0)`;
     serce[2].style.filter = `grayscale(0)`;
-    
+    serce[3].style.filter = `grayscale(100)`;
+    serce[4].style.filter = `grayscale(100)`;
    }
    if(value2==4)
    {
+    serce[0].style.filter = `grayscale(0)`;
+    serce[1].style.filter = `grayscale(0)`;
+    serce[2].style.filter = `grayscale(0)`;
     serce[3].style.filter = `grayscale(0)`;
-   
+    serce[4].style.filter = `grayscale(100)`;
    }
    if(value2==5){
+    serce[0].style.filter = `grayscale(0)`;
+    serce[1].style.filter = `grayscale(0)`;
+    serce[2].style.filter = `grayscale(0)`;
+    serce[3].style.filter = `grayscale(0)`;
     serce[4].style.filter = `grayscale(0)`;
    }
 
