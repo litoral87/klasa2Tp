c39a494588dedc20bf0c7f56c2f92a65d9849e44
litoral87 <palontom87@wp.pl>
2023-04-20 17:05:01 +0200

    javascript - dodany if

--- kod.js
+++ kod.js
@@ -1,8 +1,30 @@
 // tworzymy nasłuchiwanie zdarzenia do suwaka
 
 let suwak = document.querySelector("#suwak");
+let serce = document.querySelectorAll("img");
 suwak.addEventListener("click", ()=>{
 
-    let value = suwak.value;
-    console.log(value);
+
+    let value2 = suwak.value;
+    console.log(value2);
+   if(value2==0)
+   {
+    serce[0].style.filter = `grayscale(100)`;
+    serce[1].style.filter = `grayscale(100)`;
+
+   }
+   if(value2==1)
+   {
+    serce[1].style.filter = `grayscale(0)`;
+    serce[2].style.filter = `grayscale(100)`;
+   }
+   if(value3==1)
+   {
+    serce[2].style.filter = `grayscale(0)`;
+    serce[3].style.filter = `grayscale(100)`;
+   }
+   if(value4==1)
+   {
+    serce[3].style.filter = `grayscale(0)`;
+   }
 });
