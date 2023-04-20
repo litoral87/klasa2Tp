660c0040430cda072b417906dc1c2e7712354137
litoral87 <palontom87@wp.pl>
2023-04-20 17:30:22 +0200

    prawie gotowe

--- kod.js
+++ kod.js
@@ -7,24 +7,27 @@ suwak.addEventListener("click", ()=>{
 
     let value2 = suwak.value;
     console.log(value2);
-   if(value2==0)
-   {
-    serce[0].style.filter = `grayscale(100)`;
-    serce[1].style.filter = `grayscale(100)`;
-
-   }
    if(value2==1)
    {
-    serce[1].style.filter = `grayscale(0)`;
-    serce[2].style.filter = `grayscale(100)`;
+    serce[0].style.filter = `grayscale(0)`;
    }
    if(value2==2)
    {
-    serce[2].style.filter = `grayscale(0)`;
-    serce[3].style.filter = `grayscale(100)`;
+   
+    serce[1].style.filter = `grayscale(0)`;
    }
    if(value2==3)
    {
-    serce[3].style.filter = `grayscale(0)`;
+    serce[2].style.filter = `grayscale(0)`;
+    
    }
+   if(value2==4)
+   {
+    serce[3].style.filter = `grayscale(0)`;
+   
+   }
+   if(value2==5){
+    serce[4].style.filter = `grayscale(0)`;
+   }
+
 });
